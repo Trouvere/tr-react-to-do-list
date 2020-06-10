f7db7a1350bca925c21fd7bc94ebe0982fb8d359
Mishyn Aliaksei <trouvere777@mail.ru>
2020-06-10 21:21:52 +0300

    Updates

---
+++ precache-manifest.f80329d2d14ed095ace1b831d3fed507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6321694725154e8a46d2b16e9fb2c6d7",
+    "url": "/tr-react-to-do-list/index.html"
+  },
+  {
+    "revision": "145a911b81a9597090d7",
+    "url": "/tr-react-to-do-list/static/css/main.ca10cc11.chunk.css"
+  },
+  {
+    "revision": "7fc07f05fae917f46103",
+    "url": "/tr-react-to-do-list/static/js/2.13da169a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tr-react-to-do-list/static/js/2.13da169a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "145a911b81a9597090d7",
+    "url": "/tr-react-to-do-list/static/js/main.c1213ed6.chunk.js"
+  },
+  {
+    "revision": "0402f0de5a189f044c6d",
+    "url": "/tr-react-to-do-list/static/js/runtime-main.fbc97037.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const ADD_TASK = "ADD_TASK";
-export const REMOVE_TASK = "REMOVE_TASK";
-export const COMPLETE_TASK = "COMPLETE_TASK";
-export const ON_TOGGLE_IMPORTANT_TASK = "ON_TOGGLE_IMPORTANT_TASK";
-
-export const CHANGE_FILTER = "CHANGE_FILTER";
